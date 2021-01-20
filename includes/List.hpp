b2e509d2b5b722c38245ce84eb06c71a6790de3f
AcensJJ <uentosacens@gmail.com>
2021-01-20 15:15:01 +0100

    ...

--- includes/List.hpp
+++ includes/List.hpp
@@ -38,10 +38,10 @@ namespace ft
 			typedef typename allocator_type::const_reference const_reference;
 			typedef typename allocator_type::pointer pointer;
 			typedef typename allocator_type::const_pointer const_pointer;
-			// created for Iterator
+			// # created for Iterator #
 			typedef Link<value_type> link_type;
 			typedef link_type* link_pointer;
-			// #
+			// # end #
 			typedef Iterator<value_type, link_type> iterator;
 			typedef ConstIterator<value_type, link_type> const_iterator;
 			typedef ReverseIterator<iterator> reverse_iterator;
